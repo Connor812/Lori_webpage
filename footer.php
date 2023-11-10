4946f598b286961cf5bf5cb46bf445f6ce9b47c8
Connor812 <connorsavoy812@gmail.com>
2023-11-10 09:31:35 -0500

    revered a change

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer class="containerfluid text-center" style="background-color: black;">
     <div class="thumbnail" style="background-color: black;">
-      <img src="/images/UR logo bbg.jpg" width="30%">
+      <img src="images/UR logo bbg.jpg" width="30%">
       <div class="urblack">
         <h4>address and contact info will go here</h4>
       </div>
