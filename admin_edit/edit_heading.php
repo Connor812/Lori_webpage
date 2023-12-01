710019c32f05886ac2f03577d4d8c26e68cb6185
Connor812 <connorsavoy812@gmail.com>
2023-12-01 00:02:47 -0500

    added an error handler, if the page_num or order_num doesnt exit, then the program wont know what to do, so it sends you mack to the pages with an error message

--- admin_edit/edit_heading.php
+++ admin_edit/edit_heading.php
@@ -1,4 +1,21 @@
 <?php
     require_once("../admin-header.php");
     require_once("../connect/db.php");
+    require_once("../config-url.php");
+   
+    if (!isset($_GET["page_num"]) || !isset($_GET["order_num"])) {
+        echo "Error! Cannot edit Section";
+        header("Location: " . BASE_URL . "/admin_pages.php?error=missing_section_params");
+    }
+
+
 ?>
+
+<main>
+
+
+
+
+
+
+</main>
