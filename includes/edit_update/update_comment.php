90f5eb2b7cb168b47bd63d8cd709f183470868e7
Connor812 <connorsavoy812@gmail.com>
2023-12-14 13:42:29 -0500

    I added the function for the back end to update a comment section

---
+++ includes/edit_update/update_comment.php
@@ -0,0 +1,53 @@
+<?php 
+
+require_once("../../config-url.php");
+
+$section_name = $_POST["section_name"];
+$comment_placeholder = $_POST["comment_placeholder"];
+$page_num = $_GET["page_num"];
+$order_num = $_GET["order_num"];
+
+if (!isset($_GET["page_num"]) || !isset($_GET["order_num"])) {
+    echo "error no page_num";
+    header("Location: " . BASE_URL . "/admin_pages.php?error=missing_section_params");
+} elseif (empty($section_name) || empty($comment_placeholder)) {
+    echo "error empty input";
+    header("Location: " . BASE_URL . "/admin_edit/edit_comment.php?error=empty_input&page_num=$page_num&order_num=$order_num");
+} else {
+    // Include necessary files and initialize database connection
+    require_once '../../connect/db.php';
+    
+    // Prepare and execute the SQL statement
+    $sql = "UPDATE journal_page jp
+    JOIN comment c ON jp.id = c.section_id
+    SET jp.section_name = ?,
+        c.comment_placeholder = ?
+    WHERE jp.page_num = ? AND jp.order_num = ?";
+
+    $stmt = $mysqli->prepare($sql);
+
+    if ($stmt) {
+        // Bind parameters
+        $stmt->bind_param("ssii", $section_name, $comment_placeholder, $page_num, $order_num);
+
+        // Execute the query
+        $stmt->execute();
+
+        // Check for success
+        if ($stmt->affected_rows > 0) {
+            echo "Update successful!";
+            header("Location: " . BASE_URL . "/admin_pages.php?success=updated_success&page_num=$page_num&#$order_num");
+        } else {
+            echo "Update failed!";
+            header("Location: " . BASE_URL . "/admin_pages.php?error=updated_same&&page_num=$page_num#$order_num");
+        }
+
+        // Close the statement
+        $stmt->close();
+    } else {
+        echo "Prepare failed: " . $mysqli->error;
+    }
+
+    // Close the database connection
+    $mysqli->close();
+}
