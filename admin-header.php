7fb0698a311c602aa1b6cd58081bd4f17a085ffd
Connor812 <connorsavoy812@gmail.com>
2024-01-09 09:18:51 -0500

    added a function that checks if you are logged in with a session or not. If not, then it will take you to the log in page

--- admin-header.php
+++ admin-header.php
@@ -1,10 +1,11 @@
 <?php
-require_once 'connect/db.php';
 require_once 'config-url.php';
+require_once 'connect/db.php';
+
+session_start();
 
 // Calculate the base URL
 $protocol = empty($_SERVER['HTTPS']) ? 'http' : 'https';
-$baseUrl = $protocol . '://' . $_SERVER['HTTP_HOST'] . '/UR/';
 
 // Define stylesheets
 $stylesheets = [
@@ -24,20 +25,21 @@ $stylesheets = [
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
     <title>UR Admin</title>
     <!-- MDB icon -->
-    <link rel="icon" href="<?php echo $baseUrl; ?>img/mdb-favicon.ico" type="image/x-icon" />
+    <link rel="icon" href="<?php echo BASE_URL; ?>img/mdb-favicon.ico" type="image/x-icon" />
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
     <!-- Google Fonts Roboto -->
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" />
-    
+
     <?php
     // Output dynamic CSS links
     foreach ($stylesheets as $stylesheet) {
-        echo '<link rel="stylesheet" href="' . $baseUrl . 'css/' . $stylesheet . '" />' . PHP_EOL;
+        echo '<link rel="stylesheet" href="' . BASE_URL . 'css/' . $stylesheet . '" />' . PHP_EOL;
     }
     ?>
 </head>
+
 <body>
 
     <!-- Navbar -->
@@ -55,21 +57,43 @@ $stylesheets = [
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <!-- Navbar brand -->
                 <a class="navbar-brand mt-2 mt-lg-0" href="#">
-                    <img src="<?php echo BASE_URL ?>/images/UR logo WHiteonclear.jpg" height="60" alt="MDB Logo" loading="lazy" />
+                    <img src="<?php echo BASE_URL ?>/images/UR logo WHiteonclear.jpg" height="60" alt="MDB Logo"
+                        loading="lazy" />
                 </a>
                 <!-- Left links -->
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?php echo BASE_URL ?>/admin_pages.php">Pages</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?php echo BASE_URL ?>/admin_users.php">Users</a>
-                    </li>
+                    <?php
+                    if (isset($_SESSION["admin_username"])) {
+
+                        ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo BASE_URL ?>/admin_pages.php">Pages</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo BASE_URL ?>/admin_users.php">Users</a>
+                        </li>
+
+                    <?php
+                    }
+                    ?>
+                    <?php
+                    if (isset($_SESSION["admin_username"])) {
+
+                        ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo BASE_URL ?>admin_change_pwd.php">Change Password</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo BASE_URL ?>includes/admin_logout.inc.php">Logout</a>
+                        </li>
+
+                        <?php
+                    }
+                    ?>
                 </ul>
                 <!-- Left links -->
             </div>
         </div>
         <!-- Container wrapper -->
     </nav>
-    <!-- Navbar -->
-    
+    <!-- End Navbar -->
