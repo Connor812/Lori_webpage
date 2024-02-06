2c67ac8fb0fb8300a05b971d141a757c7c44abaa
Connor812 <connorsavoy812@gmail.com>
2024-02-06 09:35:40 -0500

    this is the display to edit bullet points

--- admin_edit/edit_bullet.php
+++ admin_edit/edit_bullet.php
@@ -1,13 +1,11 @@
 <?php
-require_once("../admin-header.php");
-require_once("../connect/db.php");
+
 require_once("../config-url.php");
-require_once("../includes/display-sections.inc.php");
-require_once("../includes/admin_errors.inc.php");
+require_once("../connect/db.php");
 
 if (!isset($_GET["page_num"]) || !isset($_GET["order_num"])) {
-    echo "Error! Cannot edit Section";
     header("Location: " . BASE_URL . "/admin_pages.php?error=missing_section_params");
+    exit;
 }
 
 $page_num = $_GET["page_num"];
@@ -16,9 +14,7 @@ $order_num = $_GET["order_num"];
 $sql = "SELECT journal_page.id, bullet.id AS bullet_id, journal_page.*, bullet.*
 FROM journal_page
 LEFT JOIN bullet ON journal_page.id = bullet.section_id
-WHERE journal_page.page_num = ? AND journal_page.order_num = ?;
-
-";
+WHERE journal_page.page_num = ? AND journal_page.order_num = ?;";
 $stmt = $mysqli->prepare($sql);
 
 if (!$stmt) {
@@ -41,7 +37,6 @@ $result = $stmt->get_result();
 
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-        // * Your Code here
 
         $bullet_id = $row["bullet_id"];
 
@@ -50,6 +45,10 @@ if ($result->num_rows > 0) {
     // ! No data found
     echo "No Data Found";
 }
+
+require_once("../admin-header.php");
+require_once("../includes/display-sections.inc.php");
+require_once("../includes/admin_errors.inc.php");
 
 ?>
 <main class="edit-main">
@@ -67,7 +66,7 @@ if ($result->num_rows > 0) {
 
             if (!$stmt) {
                 // ! Handle the case where the prepared statement could not be created
-                header("Location: ");
+                header("Location: admin_pages.php?error=failed_prepare_statement");
                 exit;
             }
 
@@ -87,17 +86,15 @@ if ($result->num_rows > 0) {
                 while ($row = $result->fetch_assoc()) {
                     $bullet_content = $row["bullet_content"];
                     $bullet_point_id = $row["id"];
-
-
-                    ?> 
-                    <input class="form-control" name="bullet_point[<?php echo $bullet_point_id ?>]" type="text" value="<?php echo $bullet_content ?>">
-                    
-                    
+                    ?>
+                    <input class="form-control" name="bullet_point[<?php echo $bullet_point_id ?>]" type="text"
+                        value="<?php echo $bullet_content ?>">
                     <?php
-            
                 }
             } else {
                 // ! No data found
+                header("Location: " . BASE_URL . "admin_pages.php?error=no_bullet_info");
+                exit;
             }
 
             ?>
